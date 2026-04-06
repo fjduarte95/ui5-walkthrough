fd3d7c2e5eb2d17a49c0b08953fa77a78346d21e
fjduarte <pakorn353@gmail.com>
2026-04-06 15:39:25 -0600

    Step 9

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,18 @@
 sap.ui.define([
-  "sap/ui/core/ComponentContainer"
-], (ComponentContainer) => {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], (Controller, MessageToast) => {
   "use strict";
 
-  new ComponentContainer({
-    name: "ui5.walkthrough",
-    settings: {
-      id: "walkthrough"
-    },
-    async: true
-  }).placeAt("content");
+  return Controller.extend("ui5.walkthrough.controller.App", {
+    onShowHello() {
+      // read msg from i18n model
+      const oBundle = this.getView().getModel("i18n").getResourceBundle();
+      const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+      // show message
+      MessageToast.show(sMsg);
+    }
+  });
 });
